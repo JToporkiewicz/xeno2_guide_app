1edb1bf44772e4a0981a15c122e13db2362e5de8
Justyna Toporkiewicz <2270645t@student.gla.ac.uk>
2021-03-12 17:36:04 +0000

    react router implementation start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import HomePage from "./HomePage";
+import { Route } from "react-router-dom";
 
 function App() {
     return(
-        <div>
-            Hi
+        <div className="container-fluid">
+            <Route path="/" exact component={HomePage} />
         </div>
-    )
+    );
 }
 
 export default App;
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage(){
+    return (
+        <div className="jumbotron">
+            <h1>Xenoblade 2 Guide to 100%</h1>
+        </div>
+    );
+}
+
+export default HomePage;
