d362e54efd2fb3d2c46ae64c370f47dd91f458f8
Justyna Toporkiewicz <2270645t@student.gla.ac.uk>
2021-08-19 21:26:31 +0100

    clean up navigations

--- src/components/ClosedNavigation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function ClosedNavigation(props){
-    return(
-        <img className="closed-navigation" src="/images/helper/closedMenu.png" alt="menu" onClick={() => props.toggleNavigation()}/>
-    )
-}
-
-export default ClosedNavigation;
