a68a31ab892bbd306c06e13516a3e8b6dedf97e6
Justyna Toporkiewicz <2270645t@student.gla.ac.uk>
2022-01-23 21:34:44 +0000

    remove unused function

--- src/components/DriverDetailsPageComponent/DriverArtDetailsComponents/DriverArtDetails.jsx
+++ src/components/DriverDetailsPageComponent/DriverArtDetailsComponents/DriverArtDetails.jsx
@@ -1,21 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 import DriverArtDetailsHeader from './DriverArtDetailsHeader';
 import DriverArtLevels from './DriverArtLevels';
 import DriverArtDescription from './DriverArtDescription';
 import InnerCollapsibleComponent from '../../CommonComponents/Containers/InnerCollapsibleComponent';
 
 function DriverArtDetails(props){
-    const [open, setOpen] = useState([]);
 
-    function toggleCollapse(key){
-        console.log(open)
-        if(!open.includes(key)){
-            setOpen([...open, key])
-        }
-        else {
-            setOpen(open.filter((i) => i !== key))
-        }
-    }
     return(
         <div className="art-details-panel">
             <DriverArtDetailsHeader weapon={props.weapon} clearArt={props.clearArt} />
