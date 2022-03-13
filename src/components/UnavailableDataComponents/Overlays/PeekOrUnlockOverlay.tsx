05aad426508264465da3ad3ba68d2e77d1f61cd8
Justyna Toporkiewicz <2270645t@student.gla.ac.uk>
2022-03-13 20:35:34 +0000

    fix overlay placements

--- src/components/UnavailableDataComponents/Overlays/PeekOrUnlockOverlay.tsx
+++ src/components/UnavailableDataComponents/Overlays/PeekOrUnlockOverlay.tsx
@@ -8,18 +8,21 @@ interface IProps {
 const PeekOrUnlockOverlay = (props:IProps) => {
   return(
     <div className="overlay">
-      <img
-        className="peek-unlock-button"
-        src="/images/helper/peek.png"
-        alt="peek"
-        onClick={() => props.toggleShow(props.name)}
-      />
-      <img
-        className="peek-unlock-button"
-        src="/images/helper/openLock.png"
-        alt="unlock"
-        onClick={() => props.updateState(props.id)}
-      />
+      <div className="center-buttons">
+        <img
+          className="peek-unlock-button"
+          src="/images/helper/peek.png"
+          alt="peek"
+          onClick={() => props.toggleShow(props.name)}
+        />
+        <img
+          className="peek-unlock-button"
+          src="/images/helper/openLock.png"
+          alt="unlock"
+          onClick={() => props.updateState(props.id)}
+        />
+      </div>
+
     </div>
   )
 }
