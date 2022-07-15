f78646445f3712ab633ce4a1fcaf5406adab5247
Justyna Toporkiewicz <2270645t@student.gla.ac.uk>
2022-07-15 20:47:02 +0100

    updated loading overlay

--- src/components/CommonComponents/Loader/Loader.tsx
+++ src/components/CommonComponents/Loader/Loader.tsx
@@ -6,8 +6,15 @@ export const LoaderView = (props: IProps) => {
   return props.loaderState.length > 0 ?
     <div className="loading-overlay">
       <div className="loading-background"/>
-      <div className="loading-text">
-        {props.loaderState[0]}
+      <div className="loading-details">
+        <img
+          src='/images/helper/Core_Crystal.webp'
+          alt='loading'
+          className="loading-icon-image"
+        />
+        <div className="loading-text">
+          {props.loaderState[0]}
+        </div>
       </div>
     </div>
     : <div />
